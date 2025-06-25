ef077ec173161d6d5ddfe666bf1ab11d8e73d19e
NicolasPrietoVargas <nicolasprietovargas@gmail.com>
2025-06-25 14:27:19 -0500

    Creación del programa

---
+++ src/main/java/com/edu/escuelaing/ARSW/MVCconSpring/MVCconSpringApplication.java
@@ -0,0 +1,13 @@
+package com.edu.escuelaing.ARSW.MVCconSpring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MVCconSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MVCconSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ARSW/MVCconSpring/MVCconSpringApplicationTests.java
@@ -0,0 +1,14 @@
+package com.ARSW.MVCconSpring;
+
+import com.edu.escuelaing.ARSW.MVCconSpring.MVCconSpringApplication;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest(classes = MVCconSpringApplication.class)
+class MVCconSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
